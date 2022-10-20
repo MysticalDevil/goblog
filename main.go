bb97d450aa34388bcb4757e2c119e1c79078ac36
MysticalDEvil <jinpeng7981@outlook.com>
2022-10-20 18:34:32 +0800

    Test http.ServeMux

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 		_, err = fmt.Fprintf(w, "<h1>Hello，这里是 goblog</h1>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-		_, err = fmt.Fprintf(w, "<h1>请求页面未找到 :(</h1>" + "<p>如有疑惑，请联系我</p>")
+		_, err = fmt.Fprintf(w, "<h1>请求页面未找到 :(</h1>"+"<p>如有疑惑，请联系我</p>")
 	}
 	if err != nil {
 		log.Println(err)
@@ -23,7 +23,7 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, err := fmt.Fprintf(w, "此博客为个人练手项目，如有反馈和建议，请联系\n" + "<a href=\"mailto:devil2gamma@gmail.com\">devil2gamma@gmail.com</a>")
+	_, err := fmt.Fprintf(w, "此博客为个人练手项目，如有反馈和建议，请联系\n"+"<a href=\"mailto:devil2gamma@gmail.com\">devil2gamma@gmail.com</a>")
 	if err != nil {
 		log.Println(err)
 	}
@@ -36,8 +36,14 @@ func main() {
 	router.HandleFunc("/about", aboutHandler)
 
 	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			_, _ = fmt.Fprintf(w, "访问文章列表")
+		case "POST":
+			_, _ = fmt.Fprint(w, "创建新的文章")
+		}
 		id := strings.SplitN(r.URL.Path, "/", 3)[2]
-		_, _ = fmt.Fprintf(w, "文章 ID：" + id)
+		_, _ = fmt.Fprintf(w, "文章 ID："+id)
 	})
 
 	err := http.ListenAndServe(":8080", router)
