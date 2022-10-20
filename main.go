ba4d4d9ee86d244f4d4b144b8fe0801780f13cc2
MysticalDEvil <jinpeng7981@outlook.com>
2022-10-20 19:48:45 +0800

    Read request data

--- main.go
+++ main.go
@@ -33,7 +33,20 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStorageHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w, "请提供正确的数据！")
+		return
+	}
+
+	title := r.PostForm.Get("title")
+
+	fmt.Fprintf(w, "POST PostForm: %v <br/>", r.PostForm)
+	fmt.Fprintf(w, "POST Form: %v <br/>", r.Form)
+	fmt.Fprintf(w, "r.Form 中的 test 值为: %v <br/>", r.FormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中的 test 值为: %v <br/>", r.PostFormValue("test"))
+	fmt.Fprintf(w, "title 的值为: %v <br/>", title)
+
 }
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,7 +57,7 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
     <title>创建文章 —— 我的技术博客</title>
 </head>
 <body>
-    <form action="%s" method="post">
+    <form action="%s?test=data" method="post">
         <p><input type="text" name="title"></p>
         <p><textarea name="body" cols="30" rows="10"></textarea></p>
         <p><button type="submit">提交</button></p>
