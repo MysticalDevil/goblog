5f509dae4a1895a4baccc76560b69b718c744bd6
MysticalDevil <jinepng7981@outlook.com>
2022-10-20 16:53:31 +0800

    Set header

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	var err error
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	var err error = nil
 	if r.URL.Path == "/" {
 		_, err = fmt.Fprintf(w, "<h1>Hello，这里是 goblog</h1>")
 	} else if r.URL.Path == "/about" {
-		_, err = fmt.Fprintf(w, "此博客为个人练手项目，如有反馈和建议，请联系" + "<a href=\"mailto:devil@gamma@gmail.com\">devil@gamma@gmail.com</a>")
+		_, err = fmt.Fprintf(w, "此博客为个人练手项目，如有反馈和建议，请联系\n" + "<a href=\"mailto:devil2gamma@gmail.com\">devil2gamma@gmail.com</a>")
 	} else {
-		_, err = fmt.Fprintf(w, "<h1>请求页面未找到</h1>")
+		_, err = fmt.Fprintf(w, "<h1>请求页面未找到 :(</h1>" + "<p>如有疑惑，请联系我</p>")
 	}
 	if err != nil {
 		return
