bd5565b3ddf4796f736b349825d15e37e61ad3ad
MysticalDevil <jinepng7981@outlook.com>
2022-10-20 16:57:28 +0800

    404 status code

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
 	var err error = nil
 	if r.URL.Path == "/" {
 		_, err = fmt.Fprintf(w, "<h1>Hello，这里是 goblog</h1>")
 	} else if r.URL.Path == "/about" {
 		_, err = fmt.Fprintf(w, "此博客为个人练手项目，如有反馈和建议，请联系\n" + "<a href=\"mailto:devil2gamma@gmail.com\">devil2gamma@gmail.com</a>")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		_, err = fmt.Fprintf(w, "<h1>请求页面未找到 :(</h1>" + "<p>如有疑惑，请联系我</p>")
 	}
 	if err != nil {
