60fb6752831f37c02d4a5dad8551b0882ddfc39e
MysticalDevil <jinepng7981@outlook.com>
2022-10-21 17:01:15 +0800

    Save blog posts to database

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 	"time"
 	"unicode/utf8"
@@ -72,11 +73,14 @@ func articlesStorageHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 检查是否有错误
 	if len(errors) == 0 {
-		fmt.Fprint(w, "验证通过！<br/>")
-		fmt.Fprintf(w, "title 的值为： %v<br/>", title)
-		fmt.Fprintf(w, "title 的长度为：%v<br/>", utf8.RuneCountInString(title))
-		fmt.Fprintf(w, "body 的值为：%v<br/>", body)
-		fmt.Fprintf(w, "body 的长度为：%v<br/>", utf8.RuneCountInString(body))
+		lastInsertID, err := saveArticleToDB(title, body)
+		if lastInsertID > 0 {
+			fmt.Fprintf(w, "插入成功，ID为"+strconv.FormatInt(lastInsertID, 10))
+		} else {
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		}
 	} else {
 		storeURL, _ := router.Get("articles.store").URL()
 
@@ -178,6 +182,35 @@ func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func saveArticleToDB(title string, body string) (int64, error) {
+	var (
+		id int64
+		err error
+		rs sql.Result
+		stmt *sql.Stmt
+	)
+	// 获取一个 prepare 声明语句
+	stmt, err = db.Prepare("INSERT INTO articles (title, body) VALUES (?, ?)")
+	if err != nil {
+		return 0, err
+	}
+	// 关闭语句，防止占用 SQL 连接
+	defer stmt.Close()
+
+	// 执行请求，传参进入绑定内容
+	rs, err = stmt.Exec(title, body)
+	if err != nil {
+		return 0, err
+	}
+
+	// 插入成功后，会返回自增 id
+	if id, err = rs.LastInsertId(); id > 0 {
+		return id, nil
+	}
+
+	return 0, err
 }
 
 func main() {
