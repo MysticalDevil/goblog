36738526498e65ec4556d934af6f48d7abfbf2e7
MysticalDevil <jinepng7981@outlook.com>
2022-10-23 19:58:54 +0800

    Login form and session control

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -45,3 +45,13 @@ func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+
+// Login 登录页面
+func (*AuthController) Login(w http.ResponseWriter, r *http.Request) {
+	view.RenderSimple(w, view.D{}, "auth.login")
+}
+
+// DoLogin 处理登录逻辑
+func (*AuthController) DoLogin(w http.ResponseWriter, r *http.Request) {
+
+}
