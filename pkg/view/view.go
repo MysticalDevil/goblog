da4831a28f28484285f6908ece72ea56df8b3811
MysticalDevil <jinepng7981@outlook.com>
2022-10-24 13:24:54 +0800

    Login status

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -1,6 +1,7 @@
 package view
 
 import (
+	"goblog/pkg/auth"
 	"html/template"
 	"io"
 	"path/filepath"
@@ -14,27 +15,20 @@ import (
 type D map[string]any
 
 // Render 渲染通用视图
-func Render(w io.Writer, data any, tplFiles ...string) {
+func Render(w io.Writer, data D, tplFiles ...string) {
 	RenderTemplate(w, "app", data, tplFiles...)
 }
 
 // RenderSimple 渲染简单的视图
-func RenderSimple(w io.Writer, data any, tplFiles ...string) {
+func RenderSimple(w io.Writer, data D, tplFiles ...string) {
 	RenderTemplate(w, "simple", data, tplFiles...)
 }
 
 // RenderTemplate 渲染视图
-func RenderTemplate(w io.Writer, name string, data any, tplFiles ...string) {
-	viewDir := "resources/views/"
+func RenderTemplate(w io.Writer, name string, data D, tplFiles ...string) {
+	data["isLogin"] = auth.Check()
 
-	for i, f := range tplFiles {
-		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
-	}
-
-	layoutFiles, err := filepath.Glob(viewDir + "layouts/*.gohtml")
-	logger.LogError(err)
-
-	allFiles := append(layoutFiles, tplFiles...)
+	allFiles := getTemplateFiles(tplFiles...)
 
 	tmpl, err := template.New("").
 		Funcs(template.FuncMap{
@@ -44,4 +38,17 @@ func RenderTemplate(w io.Writer, name string, data any, tplFiles ...string) {
 
 	err = tmpl.ExecuteTemplate(w, name, data)
 	logger.LogError(err)
+}
+
+func getTemplateFiles(tplFiles ...string) []string {
+	viewDir := "resources/views/"
+
+	for i, f := range tplFiles {
+		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
+	}
+
+	layoutFiles, err := filepath.Glob(viewDir + "layouts/*.gohtml")
+	logger.LogError(err)
+
+	return append(layoutFiles, tplFiles...)
 }
