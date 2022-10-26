a51fd965175a781e554a96f5bb57ca585fc64991
MysticalDevil <jinepng7981@outlook.com>
2022-10-26 16:15:00 +0800

    Show category

--- routes/web.go
+++ routes/web.go
@@ -47,6 +47,7 @@ func RegisterWebRoutes(r *mux.Router) {
 	cc := new(controllers.CategoriesController)
 	r.HandleFunc("/categories/create", middlewares.Auth(cc.Create)).Methods("GET").Name("categories.create")
 	r.HandleFunc("/categories", middlewares.Auth(cc.Store)).Methods("POST").Name("categories.store")
+	r.HandleFunc("/categories/{id:[0-9]+}", middlewares.Auth(cc.Show)).Methods("GET").Name("categories.show")
 
 	// 静态资源
 	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
